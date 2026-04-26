39c89949327cb93559a2f792e71b0c3c79ced0e5
Godzap <enzo.baiao123@gmail.com>
2026-04-25 22:29:46 -0300

    teste

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -63,13 +63,13 @@ export default function Layout() {
       <Navbar />
 
       <main className="flex-1 flex flex-col px-8 pt-6 pb-2 overflow-y-auto min-h-0">
-        <AnimatePresence mode="wait">
+        <AnimatePresence>
           <motion.div
             key={location.pathname}
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -6 }}
-            transition={{ duration: 0.22, ease: 'easeOut' }}
+            transition={{ duration: 0.18, ease: 'easeOut' }}
             className="flex-1 flex flex-col min-h-0"
           >
             <FrozenRoute />
